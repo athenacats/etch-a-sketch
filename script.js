c1406e4da89918fef994530a845d511c5389327a
athenacats <124017569+athenacats@users.noreply.github.com>
2023-03-07 15:17:01 +0300

    Add div row1

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const div = document.createElement('div')
-div.classList.add('.container')
+div.classList.add('container')
 document.body.appendChild(div)
 
+const row1 = document.createElement('div')
+row1.classList.add('row1');
+div. appendChild(row1)
