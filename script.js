0733cd438caf6f4174520975545e31789fe64002
athenacats <124017569+athenacats@users.noreply.github.com>
2023-03-07 15:20:22 +0300

    Add columns to row1

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ document.body.appendChild(div)
 const row1 = document.createElement('div')
 row1.classList.add('row1');
 div. appendChild(row1)
+
+const col1 = document.createElement('div')
+col1.classList.add('col1');
+row1. appendChild(col1)
+
+const col2 = document.createElement('div')
+col2.classList.add('col2');
+row1. appendChild(col2)
+
+const col3 = document.createElement('div')
+col3.classList.add('col3');
+row1. appendChild(col3)
+
+const col4 = document.createElement('div')
+col4.classList.add('col4');
+row1. appendChild(col4)
